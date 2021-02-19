a6aa2bcdb4ef01c476d65a08789d31c0acdb2af3
jfletcher98 <jofl3978@colorado.edu>
2021-02-18 23:13:58 -0700

    Started addingShip implementation

--- src/edu/colorado/teamc/Grid.java
+++ src/edu/colorado/teamc/Grid.java
@@ -1,5 +1,7 @@
 package edu.colorado.teamc;
 
+import java.util.Vector;
+
 enum Tile {
     HIT,
     EMPTY,
@@ -20,25 +22,36 @@ public class Grid {
 
     }
 
-    public String getTileType(Coordinate c){
-        switch(grid[c.getRow()][c.getCol()]){
-            case EMPTY:
-                return "EMPTY";
-        }
-        return null;
+    public Tile getTileType(Coordinate c){
+        return grid[c.getRow()][c.getCol()];
     }
 
-    public boolean addShip(Ship ship, Coordinate coordinate){
+    public boolean addShip(Ship ship, Vector<Coordinate> coordinates, Character orientation){
 
         // TODO: implement this function (John)
         // search grid and make sure it's possible to add the ship
 
+        // TODO: Check if vector coordiantes are horizontal or vertical
+
+        //For now, assume the vector makes a horizontal or vertical line
+        for(Coordinate coord : coordinates) {
+            Tile tileType = getTileType(coord);
+            //Check if any coordinate goes off the board
+            if(coord.getRow() < 0 || coord.getCol() < 0 || coord.getRow() > 10 || coord.getCol() > 10) {
+                return false;
+            }
+            if(tileType != Tile.EMPTY) {
+                return false;
+            }
+            grid[coord.getRow()][coord.getCol()] = Tile.OCCUPIED;
+            ship.updateCoordinates(coord);
+        }
         // if not possible return false
 
         // if the ship is added, update the Tile enums and return true, and then call
         // ship.updateCoordinates(Coordinate coordinate);
 
-        return false;
+        return true;
 
     }
 }
